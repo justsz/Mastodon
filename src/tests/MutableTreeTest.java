525e1ea603b826608c1df9751410e9a06bf38bb2
Justs <justs.zarins@gmail.com>
2012-05-25 17:26:20 +0100

    Improved Nexus Reader/Writer. More test cases. Added JavaDoc.
    NexusReader can now read in MutableRootedTree and CompactRootedTree
    objects.

---
+++ src/tests/MutableTreeTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import java.io.IOException;
+import java.util.List;
+
+import jebl.evolution.io.ImportException;
+import jebl.evolution.trees.MutableRootedTree;
+import jebl.evolution.trees.RootedTree;
+import jebl.evolution.trees.RootedTreeUtils;
+import jebl.evolution.trees.SimpleRootedTree;
+import core.NexusReader;
+import core.NexusWriter;
+
+public class MutableTreeTest {
+	public static void main(String[] args) throws IOException, ImportException{
+		String test = "test4.nex";
+		NexusReader reader = new NexusReader(test);
+
+		List<MutableRootedTree> mutableTrees = reader.readMutableRootedTrees();
+
+		reader.reset();
+		List<SimpleRootedTree> simpleTrees = reader.readSimpleRootedTrees();
+
+		System.out.print("Mutable rooted trees equal to simple rooted trees: ");
+		for(int i = 0; i < mutableTrees.size(); i++) {
+			if (!RootedTreeUtils.equal(mutableTrees.get(i), (RootedTree) simpleTrees.get(i)))
+				System.out.println(false);
+			else 
+				System.out.println(true);
+		}
+		
+		NexusWriter writer = new NexusWriter("out.nex");
+		writer.writeTrees(mutableTrees);
+		
+	}
+}
--- src/tests/ReadWriteTest.java
+++ src/tests/ReadWriteTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,10 +12,10 @@ public class ReadWriteTest {
 	public static void main(String[] args) throws IOException, ImportException {
 		String test = "test4.nex";
 		NexusReader reader = new NexusReader(test);
-		List<Tree> trees1 = reader.readTrees();
+		List<SimpleRootedTree> trees1 = reader.readSimpleRootedTrees();
 
-		reader.setFile(test);
-		List<Tree> trees2 = reader.readTrees();
+		reader.reset();
+		List<SimpleRootedTree> trees2 = reader.readSimpleRootedTrees();
 		
 		//Test if reading in the same trees twice creates two equal tree objects.
 		for(int i = 0; i < trees1.size(); i++) {
@@ -29,9 +28,9 @@ public class ReadWriteTest {
 		writer.writeTrees(trees1);
 		
 		reader.setFile(out);
-		List<Tree> trees3 = reader.readTrees();
+		List<SimpleRootedTree> trees3 = reader.readSimpleRootedTrees();
 		
-		//Test if a tree read, written, read again matches itself unchanged.
+		//Test if a tree read, written, read again matches itself.
 		for(int i = 0; i < trees1.size(); i++) {
 			System.out.print("Tree read, written, read again equal to itself: ");
 			System.out.println(RootedTreeUtils.equal((RootedTree) trees1.get(i), (RootedTree) trees3.get(i)));
