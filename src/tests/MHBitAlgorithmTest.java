be1af6f2d1bea8200b2231c091f144cde35810b1
Justs <justs.zarins@gmail.com>
2012-06-13 11:37:51 +0100

    DELETED the algorithm that worked on simple tree objects.

--- src/tests/MHBitAlgorithmTest.java
+++ src/tests/MHBitAlgorithmTest.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import algorithms.MHAlgorithm;
 import algorithms.MHBitAlgorithm;
 
 import jebl.evolution.io.ImportException;
